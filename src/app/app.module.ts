afced04504d45033fb4f37627e017e55db7fbdc6
yannai <fischeryannai@gmail.com>
2023-01-03 20:36:49 +0100

    renamed project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CriteriaComponent } from './components/criteria/criteria.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CriteriaComponent} from './components/criteria/criteria.component';
 import {PrimaryComponent} from "./components/primary/primary.component";
 import {CompanyBusinessCardComponent} from "./components/company-business-card/company-business-card.component";
 import {SettingsComponent} from "./components/settings/settings/settings.component";
